227f74d7a0d95dd1832a1550b80258e29471cc39
tomo.eguchi@noaa.gov <tomo.eguchi@noaa.gov>
2018-05-02 10:19:25 -0700

    minor _fcns.R

--- Dc_Indonesia_nesting_fcns.R
+++ Dc_Indonesia_nesting_fcns.R
@@ -5,16 +5,11 @@
 
 #sysInfo <- Sys.info()
 ifelse(Sys.info()[1] == 'Linux',
-       source('~/Documents/R/TomosFunctions.R'),
-       source('~/R/TomosFunctions.R'))
+       source('~/Documents/R/tools/TomosFunctions.R'),
+       source('~/R/tools/TomosFunctions.R'))
 
 library(ggplot2)
 library(tidyverse)
 library(lubridate)
 
-mmm2month <- function(x){
-  switch(as.character(x),
-         "Jan" = 1, "Feb" = 2, "Mar" = 3, "Apr" = 4,
-         "May" = 5, "Jun" = 6, "Jul" = 7, "Aug" = 8,
-         "Sep" = 9, "Oct" = 10, "Nov" = 11, "Dec" = 12, NA)
-}
+
